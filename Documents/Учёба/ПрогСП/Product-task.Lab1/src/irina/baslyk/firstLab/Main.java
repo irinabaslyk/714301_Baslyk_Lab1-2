e84af740da46d8cb93a97d3df85a7d662d24b14d
irinabaslyk <baslyk.irina@gmail.com>
2019-10-23 21:07:15 +0300

    t22

---
+++ Documents/Учёба/ПрогСП/Product-task.Lab1/src/irina/baslyk/firstLab/Main.java
@@ -0,0 +1,11 @@
+package irina.baslyk.firstLab;
+
+public class Main {
+    public static void main(String[] args){
+        Product P1 = new Product();
+        P1.PrintHead();
+        P1.PrintAll();
+        WriterInfo W1 = new WriterInfo();
+        W1.Product();
+    }
+}
---
+++ Documents/Учёба/ПрогСП/Product-task.Lab1/src/irina/baslyk/firstLab/WriterInfo.java
@@ -0,0 +1,8 @@
+package irina.baslyk.firstLab;
+
+public class WriterInfo {
+    public void Product(){
+        Product PW = new Product("Йогурт Danone",124,1.25);
+        PW.PrintAll();
+    }
+}
