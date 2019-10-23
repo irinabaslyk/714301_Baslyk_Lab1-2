97fcba9ebe122a63195b77e7a9f89c39de40ef57
irinabaslyk <baslyk.irina@gmail.com>
2019-10-23 21:06:32 +0300

    t2

--- Documents/Учёба/ПрогСП/Product-task.Lab1/src/irina/baslyk/firstLab/WriterInfo.java
+++
@@ -1,8 +0,0 @@
-package irina.baslyk.firstLab;
-
-public class WriterInfo {
-    public void Product(){
-        Product PW = new Product("Йогурт Danone",124,1.25);
-        PW.PrintAll();
-    }
-}
